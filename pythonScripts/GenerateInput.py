46ee331651ad82ddbf62171e6b20138fc6cb1e56
desaulniersm <mdesaulniersm@gmail.com>
2022-04-22 14:46:07 -0600

    Added input-param file

--- pythonScripts/GenerateInput.py
+++ pythonScripts/GenerateInput.py
@@ -3,9 +3,12 @@
 
 # GraphGenerator currently generates 2D graph only
 
+import sys
+import random
+
 class Node:
     def __init__(self, i, j, MaxWeight, GraphDim):
-        self.coordinates = "\n(" + str(i) + "," + str(j) + ") "
+        self.coordinates = "\n" + str(i) + "," + str(j) + " "
         self.Weights = ""
         self.AgentUpdateValues = "(0,0,0)"
         self.GraphDim = GraphDim
@@ -13,17 +16,21 @@ class Node:
         self.i = i
         self.j = j
     def GenerateWeights(self):
-        di = self.MaxWeight / self.GraphDim[0]
-        dj = self.MaxWeight / self.GraphDim[1]
+        di = float(self.MaxWeight) / float(self.GraphDim[0])
+        dj = float(self.MaxWeight) / float(self.GraphDim[1])
 
         #w1 goes linearly with i (left to right)
         w1 = round(di * self.i, 3)
         #w2 goes linearly with j (up and down)
         w2 = round(dj * self.j, 3)
         #w3 goes linearly with the average of i and j (diagonal)
-        w3 = round((w1 + w2) / 2, 3)
+        w3 = round((w1 + w2) / 2.0, 3)
 
-        self.Weights = "(" + str(w1) + "," + str(10) + "," + str(w3) + ") "
+        mu = 0
+        sigma = 1 / (self.MaxWeight*2)
+
+        self.Weights = "" + str(w1) + "," + str(w2) + "," + str(w3) + " "
+        self.AgentUpdateValues = "" + str( round(w1/10.0 + random.gauss(mu, sigma),3)) + "," + str( round(w2/10.0 + random.gauss(mu, sigma),3)) + "," + str( round(w3/10.0 + random.gauss(mu, sigma),3)) + ""
 
     # def GenerateAgentUpdate(self:)
 
@@ -36,26 +43,55 @@ class Node:
 
 class GraphGenerator:
 
-    def __init__(self, MaxWeight, WeightDim, GraphDim):
-        self.MaxWeight = MaxWeight
+    def __init__(self, MinMaxWeight, WeightDim, GraphDim1, GraphDim2):
+        # self.MinWeight = MinMaxWeight[0]
+        # self.MaxWeight = MinMaxWeight[1]
+        self.MaxWeight = MinMaxWeight
         self.WeightDim = WeightDim
-        self.GraphDim = GraphDim
+        self.GraphDim1 = GraphDim1
+        self.GraphDim2 = GraphDim2
         self.Lines = []
         self.f = open('InputGraph.txt', 'w')
 
     def GenerateLines(self):
-        for i in range(self.GraphDim[0]):
-            for j in range(self.GraphDim[1]):
-                ThisNode = Node(i, j, self.MaxWeight, self.GraphDim)
-                self.Lines.append(ThisNode.MakeNode())
 
+        # Add Graph Dimensions to top of file
+        Line = str(GraphDim1[0])+","+str(GraphDim1[1])
+        self.Lines.append(Line)
+        Line = "\n"+str(GraphDim2[0])+","+str(GraphDim2[1])
+        self.Lines.append(Line)
+        
+        
+        GraphSpans = [GraphDim1[1]-GraphDim1[0], GraphDim2[1]-GraphDim2[0]]
+        
+        for i in range(self.GraphDim1[0], self.GraphDim1[1], 1):
+            for j in range(self.GraphDim2[0], self.GraphDim2[1], 1):
+                ThisNode = Node(i, j, self.MaxWeight, GraphSpans)
+                self.Lines.append(ThisNode.MakeNode())
 
     def GenerateGraph(self):
         self.GenerateLines()
         self.f.writelines(self.Lines)
         self.f.close()
 
+# Output to graph 
+# State Bounds (min, max)
+# Graph Bounds (minDim1, maxDim1) \n (minDim2, maxDim2)
+# Graph type
+# WeightBounds = [float(sys.argv[0]), float(sys.argv[1]) ]
+# graphBounds = [sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5] ]
+# graphType = str(sys.argv[6])
 
 
-InputGraph = GraphGenerator(10, 3, (10,30))
+# GraphGenerator starts with (List (state weight bounds), List())
+MaxWeight = 10
+WeightDim = 3
+GraphDim1 = (0,10)
+GraphDim2 = (-5, 20)
+
+InputParameters = sys.argv[0]
+InputGraph = GraphGenerator(MaxWeight, WeightDim, GraphDim1, GraphDim2)
 InputGraph.GenerateGraph()
+
+
+# AgentGenerator 
